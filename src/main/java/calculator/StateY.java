ab4f87f875f141d520eab3a3cbba0c484043b825
VladimirGlinskikh <support@zhelezyaka.kz>
2020-02-10 00:02:55 +0600

    broke the task into sixteen parts

---
+++ src/main/java/calculator/StateY.java
@@ -0,0 +1,23 @@
+package calculator;
+
+public class StateY extends State {
+    @Override
+    void clear(Context context) {
+
+    }
+
+    @Override
+    void digit(Context context, char key) {
+
+    }
+
+    @Override
+    void arifm(Context context, char key) {
+
+    }
+
+    @Override
+    void equal(Context context) {
+
+    }
+}
