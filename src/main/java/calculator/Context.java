c2f84a27093f0eb12a8f75848720688618f54051
VladimirGlinskikh <support@zhelezyaka.kz>
2020-02-09 23:45:28 +0600

    write method press()

--- src/main/java/calculator/Context.java
+++ src/main/java/calculator/Context.java
@@ -7,7 +7,32 @@ public class Context {
     State state;
 
     public void press(char key) {
-
+        switch (key) {
+            case 'c':
+            case 'C':
+                state.clear(this);
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                state.digit(this, key);
+                break;
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                state.arifm(this, key);
+                break;
+            case '=':
+                state.equal(this);
+        }
     }
 
     public int run(String keys) {
