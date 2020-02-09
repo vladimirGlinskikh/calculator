39c5e3685912c582959fa39cbc740cf6c47db6af
VladimirGlinskikh <support@zhelezyaka.kz>
2020-02-09 23:35:55 +0600

    first stage of the test. Wrote a test and met a red bar

--- src/main/java/calculator/Context.java
+++ src/main/java/calculator/Context.java
@@ -4,8 +4,15 @@ public class Context {
     int x;
     int y;
     char o;
+    State state;
 
-    public void press(char key){
+    public void press(char key) {
 
+    }
+
+    public int run(String keys) {
+        for (char key : keys.toCharArray())
+            press(key);
+        return x;
     }
 }
---
+++ src/test/java/calculator/ContextTest.java
@@ -0,0 +1,31 @@
+package calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ContextTest {
+
+    @Test
+    public void run() {
+        Context context = new Context();
+//        int actual = context.run("с2+3");
+//        int expect = 6;
+//        assertEquals(expect, actual);
+        assertEquals(0, context.run("C"));
+        assertEquals(2, context.run("C2"));
+        assertEquals(25, context.run("C25"));
+        assertEquals(6, context.run("с2+3"));
+        assertEquals(343256, context.run("C343256"));
+        assertEquals(777, context.run("C123+654="));
+        assertEquals(8, context.run("C2+2*2="));
+        assertEquals(10, context.run("C5+="));
+        assertEquals(200, context.run("C250-50="));
+        assertEquals(20, context.run("C100/5="));
+        assertEquals(56, context.run("C7*8="));
+        assertEquals(0, context.run("C45*45="));
+        assertEquals(5625, context.run("C75*75="));
+        assertEquals(4, context.run("C2*2="));
+        assertEquals(1, context.run("C9/5="));
+    }
+}
