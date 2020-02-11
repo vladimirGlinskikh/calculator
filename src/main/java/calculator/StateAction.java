0972aba1b4eb97d05533cf9c7500855e3c5991ec
VladimirGlinskikh <support@zhelezyaka.kz>
2020-02-11 11:03:56 +0600

    class StateAction is ready

--- src/main/java/calculator/StateAction.java
+++ src/main/java/calculator/StateAction.java
@@ -3,21 +3,24 @@ package calculator;
 public class StateAction extends State {
     @Override
     void clear(Context context) {
-
+        context.state = new StateX();
+        context.state.clear(context);
     }
 
     @Override
     void digit(Context context, char key) {
-
+        context.state = new StateY();
+        context.state.digit(context, key);
     }
 
     @Override
     void arifm(Context context, char key) {
-
+        context.o = key;
     }
 
     @Override
     void equal(Context context) {
-
+        context.state = new StateAnswer();
+        context.state.equal(context);
     }
 }
