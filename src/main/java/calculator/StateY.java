fe57a300d2def092dc7dc084f127005c34178724
VladimirGlinskikh <support@zhelezyaka.kz>
2020-02-11 13:32:00 +0600

    class StateY is ready

--- src/main/java/calculator/StateY.java
+++ src/main/java/calculator/StateY.java
@@ -3,21 +3,24 @@ package calculator;
 public class StateY extends State {
     @Override
     void clear(Context context) {
-
+        context.state = new StateX();
+        context.state.clear(context);
     }
 
     @Override
     void digit(Context context, char key) {
-
+        context.y = context.y * 10 + Character.getNumericValue(key);
     }
 
     @Override
     void arifm(Context context, char key) {
-
+        equal(context);
+        context.state.arifm(context, key);
     }
 
     @Override
     void equal(Context context) {
-
+        context.state = new StateAnswer();
+        context.state.equal(context);
     }
 }
