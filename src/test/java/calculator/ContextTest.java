261f08aef17d02edf399c3d063d325c57c2b7068
VladimirGlinskikh <support@zhelezyaka.kz>
2020-02-12 22:31:20 +0600

    test is green

--- src/test/java/calculator/ContextTest.java
+++ src/test/java/calculator/ContextTest.java
@@ -13,16 +13,16 @@ public class ContextTest {
         assertEquals(2, context.run("c2"));
         assertEquals(25, context.run("c25"));
         assertEquals(5, context.run("c2+3="));
-        assertEquals(343256, context.run("C343256"));
-        assertEquals(777, context.run("C123+654="));
+        assertEquals(343256, context.run("c343256"));
+        assertEquals(777, context.run("c123+654="));
         assertEquals(8, context.run("c2+2*2="));
-        assertEquals(10, context.run("C5+="));
-        assertEquals(200, context.run("C250-50="));
-        assertEquals(20, context.run("C100/5="));
-        assertEquals(56, context.run("C7*8="));
-        assertEquals(0, context.run("C45*45="));
-        assertEquals(5625, context.run("C75*75="));
-        assertEquals(4, context.run("C2*2="));
-        assertEquals(1, context.run("C9/5="));
+        assertEquals(10, context.run("c5+="));
+        assertEquals(200, context.run("c250-50="));
+        assertEquals(20, context.run("c100/5="));
+        assertEquals(56, context.run("c7*8="));
+        assertEquals(2025, context.run("c45*45="));
+        assertEquals(5625, context.run("c75*75="));
+        assertEquals(4, context.run("c2*2="));
+        assertEquals(1, context.run("c9/5="));
     }
 }
